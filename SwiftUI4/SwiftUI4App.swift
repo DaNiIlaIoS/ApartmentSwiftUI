1107d131e18606e4785df58c60095d828928000e
Daniil Sivozhelezov <sivozelezovdaniil@gmail.com>
2024-08-15 16:41:15 +0500

    Initial Commit

---
+++ SwiftUI4/SwiftUI4App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI4App.swift
+//  SwiftUI4
+//
+//  Created by Даниил Сивожелезов on 15.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
